3a4bb46ea1f7fa8054f69f351b60879277a5c1df
crhenderson <coruhen@gmail.com>
2014-12-20 20:46:06 -0600

    add run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,102 @@
+################################################################################################################################
+## File: run_analysis.R                                                                                                       ##
+##                                                                                                                            ##
+## Instructions:                                                                                                              ##
+## You should create one R script called run_analysis.R that does the following.                                              ##
+##  1. Merges the training and the test sets to create one data set.                                                          ##
+##  2. Extracts only the measurements on the mean and standard deviation for each measurement.                                ##
+##  3. Uses descriptive activity names to name the activities in the data set                                                 ##
+##  4. Appropriately labels the data set with descriptive variable names.                                                     ##
+##  5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable             ##
+##     for each activity and each subject.                                                                                    ##
+################################################################################################################################
+
+## Requires Package "dplyr" and "tidyr".  The following function checks if package is installed.  
+usePackage <- function(p) {
+  if (!is.element(p, installed.packages()[,1])){
+    stop(paste("Program requires package:",p,". Please install and try again",sep=" "))}
+  require(p, character.only = TRUE)
+}  
+
+
+  ## Create function to get file name and path from directory and read data into a table
+  ReadTable <- function(file, set=NA, col_names = vector()) {
+    dir<-"./UCI HAR Dataset"
+    fname <- file
+    fpath <- if(is.na(set)){file.path(dir,fname)} else {file.path(dir,set,fname)}
+    data <- read.table(fpath, header=F)
+    if(length(col_names)!=0) {names(data) <- col_names}
+    data
+  }
+  
+  ##Read training or test data set.  Leaving only the mean and std columns.  Appends the subject information and the activity labels to the front
+  ##of the data set
+  #dataset = "train" or "test"
+  GetData <- function(dataset) {
+    #Get Activity Labels and Names
+    activitylabels <- ReadTable(file="activity_labels.txt",col_names = c("activityid","activity"))
+    #Get feature names
+    features <- ReadTable(file="features.txt",col_names=c("featureid","featurename"))
+    #Get Data Set
+    lookups <- c(paste('/X_',dataset,'.txt',sep=''),paste('/y_',dataset,'.txt',sep=''),paste('/subject_',dataset,'.txt',sep=''))
+    data <- ReadTable(file=lookups[1],set=dataset,col_names=features$featurename)
+    #Extract only measures of mean and standard deviation.
+    mean_std <- sort(c(grep('std()',features$featurename,fixed=T),grep('mean()',features$featurename,fixed=T)))
+    data <- data[,mean_std]
+    #Add Activity ID and Labels to data
+    data <- cbind(ReadTable(file=lookups[2],set=dataset,col_names="activity"),data)
+    #Add Subject
+    data <- cbind(ReadTable(file=lookups[3],set=dataset,col_names="subject"),data)
+    #Make Subject and Activity factors
+    data$subject <- factor(data$subject)
+    #Use labels to show actual activity names
+    data$activity <- factor(data$activity, labels = activitylabels$activity)
+    data
+  }  
+  
+  #Merge the data sets to create first tidydataset
+  MergeData <- function(){
+    #Read train and test sets
+    print("Reading Training set...")
+    train <- GetData('train')
+    print("Reading Test set...")
+    test <- GetData('test')
+    #rbind the two sets together
+    print("Combining data...")
+    data <- rbind(train,test)
+    #Clean Up Columns Names
+    col.names <- colnames(data)
+    col.names <- gsub("mean()","Mean",col.names,fixed=T)
+    col.names <- gsub("std()","Std",col.names,fixed=T)
+    col.names <- gsub("-","",col.names,fixed=T)
+    colnames(data)<-col.names
+    data
+  }
+  
+  # Create second tidy of means
+  SecondDataSet <- function(){
+    #create tbl_df of tidydataset1.  This is a function of dplyr
+    usePackage("dplyr")
+    usePackage("tidyr")
+    data <- tbl_df(MergeData())
+    print("Calculating means...")
+    data <- arrange(data,subject,activity)%>%
+      group_by(subject, activity)%>%
+      summarise_each(funs(mean))%>%
+      arrange(subject,activity)
+    col.names <- colnames(data)
+    col.names[!(col.names %in% c('subject','activity'))] <- paste('Mean',col.names[!(col.names %in% c('subject','activity'))],sep="")
+    colnames(data)<-col.names
+    data
+  }
+  
+  #Execute. Create tidydata.txt
+  Execute <- function(){
+    data <- SecondDataSet()
+    print("Writing tidydata.txt to working directory...")
+    write.table(data,"tidydata.txt",row.names=F)
+    print("Complete.")
+  }
+  
+suppressWarnings(Execute())
+  
